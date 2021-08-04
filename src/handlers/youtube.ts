ded39b68ba2b6fb2564c29988de6becb661c3a5d
xperia3 <88239736+xperia3@users.noreply.github.com>
2021-08-04 07:41:01 +0530

    Update youtube.ts

--- src/handlers/youtube.ts
+++ src/handlers/youtube.ts
@@ -3,7 +3,7 @@ import { commandExtractor } from '../utils';
 import ytsearch from 'yt-search';
 import { playOrQueueSong } from '../tgcalls';
 
-export const YTPlay = Composer.command(['youtube', 'yt'], async (ctx) => {
+export const YTPlay = Composer.command(['play', 'yt'], async (ctx) => {
 
     await ctx.telegram.sendChatAction(ctx.chat.id, "typing");
     
